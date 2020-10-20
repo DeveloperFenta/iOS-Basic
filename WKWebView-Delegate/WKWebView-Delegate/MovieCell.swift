8e38f5f7065dff58917986aa90b63b04326fd8d2
DeveloperFenta <fenta0917@gmail.com>
2020-10-20 20:20:52 +0900

    Add WKWebView-Delegate Project

---
+++ WKWebView-Delegate/WKWebView-Delegate/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCell.swift
+//  WKWebView-Delegate
+//
+//  Created by Fenta on 2020/10/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet var title: UILabel!
+    
+    @IBOutlet var desc: UILabel!
+    
+    @IBOutlet var rating: UILabel!
+    
+    @IBOutlet var thumbnail: UIImageView!
+    
+}
---
+++ WKWebView-Delegate/WKWebView-Delegate/MovieVO.swift
@@ -0,0 +1,19 @@
+//
+//  MovieVO.swift
+//  WKWebView-Delegate
+//
+//  Created by Fenta on 2020/10/20.
+//
+
+import Foundation
+import UIKit
+
+class MovieVO {
+    var thumbnail: String?
+    var thumbnailImage: UIImage?
+    var title: String?
+    var desc: String?
+    var detail: String?
+    var opendate: String?
+    var rating: Double?
+}
